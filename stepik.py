3b0063acf16606ea679b55e931fd08dda7324b5a
Hamit <42816049+Hamitka@users.noreply.github.com>
2020-07-01 17:03:17 +0500

    Update stepik.py
    подстановочный шифр

--- stepik.py
+++ stepik.py
@@ -1,33 +1,54 @@
-def game():
-    listofdictgame = []
-    n = int(input())
-    for i in range(n):
-        c1, g1, c2, g2 = input().split(';')
-        listcommand = [c1, c2]
-        listgoal = [int(g1), int(g2)]
-        # dictgame = dict(zip(listgame[i][::2], listgame[i][1::2]))
-        listofdictgame += [dict(zip(listcommand, listgoal))]
-    setgame = set([item for d in listofdictgame for item in d])
-    dictgame = {key: [0,0,0,0,0] for key in setgame}
-    for key1, key2 in [d.items() for d in listofdictgame]:  #каким то макаром получили списки из key+value
-        dictgame[key1[0]][0] = dictgame[key1[0]][0]+1   #всего игр 1я команда
-        dictgame[key2[0]][0] = dictgame[key2[0]][0]+1   #всего игр 2я команда
-        #здесь и далее подсчет результатов игры:
-        if key1[1] > key2[1]:
-            dictgame[key1[0]][1] = dictgame[key1[0]][1] + 1     #Побед 1я команда
-            dictgame[key2[0]][3] = dictgame[key2[0]][3] + 1     #Поражений 2я команда
-            dictgame[key1[0]][4] = dictgame[key1[0]][4] + 3    #Очков 1я команда
-        elif key1[1] < key2[1]:
-            dictgame[key2[0]][1] = dictgame[key2[0]][1] + 1      #Побед 2я команда
-            dictgame[key1[0]][3] = dictgame[key1[0]][3] + 1     #Поражений 1я команда
-            dictgame[key2[0]][4] = dictgame[key2[0]][4] + 3    #Очков 2я команда
-        else:
-            dictgame[key1[0]][2] = dictgame[key1[0]][2] + 1      #Ничья 1я команда
-            dictgame[key1[0]][4] = dictgame[key1[0]][4] + 1    #Очков 1я команда
-            dictgame[key2[0]][2] = dictgame[key2[0]][2] + 1      #Ничья 2я команда
-            dictgame[key2[0]][4] = dictgame[key2[0]][4] + 1    #Очков 2я команда
-    # print(listofdictgame)
-    for key, val in dictgame.items():
-        print ((key+':'), *val, end='\n')
-    return dictgame
-game()
+'''В какой-то момент в Институте биоинформатики биологи перестали понимать, что говорят информатики: они говорили каким-то странным набором звуков.
+
+В какой-то момент один из биологов раскрыл секрет информатиков: они использовали при общении подстановочный шифр, т.е. заменяли каждый символ исходного сообщения на соответствующий ему другой символ. Биологи раздобыли ключ к шифру и теперь нуждаются в помощи:
+
+Напишите программу, которая умеет шифровать и расшифровывать шифр подстановки. Программа принимает на вход две строки одинаковой длины, на первой строке записаны символы исходного алфавита, на второй строке — символы конечного алфавита, после чего идёт строка, которую нужно зашифровать переданным ключом, и ещё одна строка, которую нужно расшифровать.
+
+Пусть, например, на вход программе передано:
+abcd
+*d%#
+abacabadaba
+#*%*d*%
+
+Это значит, что символ a исходного сообщения заменяется на символ * в шифре, b заменяется на d, c — на % и d — на #.
+Нужно зашифровать строку abacabadaba и расшифровать строку #*%*d*% с помощью этого шифра. Получаем следующие строки, которые и передаём на вывод программы:
+*d*%*d*#*d*
+dacabac
+
+Sample Input 1:
+
+abcd
+*d%#
+abacabadaba
+#*%*d*%
+
+Sample Output 1:
+
+*d*%*d*#*d*
+dacabac
+
+Sample Input 2:
+
+dcba
+badc
+dcba
+badc
+
+Sample Output 2:
+
+badc
+dcba'''
+
+
+def shifr(decode, code, listorigin):
+    dictifcode = dict(zip(decode, code))
+    listcode = [dictifcode[key] for key in listorigin]
+    return listcode
+
+listdecode = [i for i in input()]
+listcode = [i for i in input()]
+listorigin = [i for i in input()]
+listshifr = [i for i in input()]
+
+print(*shifr(listdecode, listcode, listorigin), sep='')
+print(*shifr(listcode, listdecode, listshifr), sep='')
