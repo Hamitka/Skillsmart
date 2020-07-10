56f992414a13f6a603430decd0157f3cc3db247d
Hamit <hamitka@list.ru>
2020-07-10 21:41:12 +0500

    Salary

--- skillsmart_free.py
+++ skillsmart_free.py
@@ -1,29 +1,9 @@
-def ConquestCampaign(N, M, L, list):
-    pole = [[0 for x in range(M)] for y in range(N)]
-    batx = [i - 1 for i in list[::2]]
-    baty = [i - 1 for i in list[1::2]]
+def SynchronizingTables(N, listID, listSalary):
+    dictOfSalary = dict(zip(sorted(listID), sorted(listSalary)))
+    return [dictOfSalary[i] for i in listID]
 
-    for i in range(len(batx)):
-        pole[batx[i]][baty[i]] = 1
-
-    for d in range(1, N + M - 1):
-        for i in range(N):
-            for j in range(M):
-                if pole[i][j] == d:
-                    for di in range(-1, 2, 2):
-                        ai = i + di
-                        if 0 <= ai < N and pole[ai][j] == 0:
-                            pole[ai][j] = d + 1
-                    for dj in range(-1, 2, 2):
-                        aj = j + dj
-                        if 0 <= aj < M and pole[i][aj] == 0:
-                            pole[i][aj] = d + 1
-    # [print(i) for i in pole]
-    return max([max(i) for i in pole])
-
-# N = 30
-# M = 4
-# L = 2
-# battalion = [2,2, 3,4]
+# listOfID = [50, 1, 1024, 2]
+# listOfSalary = [20000, 100000, 90000, 20000]
 #
-# print (ConquestCampaign(N, M, L, battalion))
+# print(SynchronizingTables(3, listOfID, listOfSalary))
+
