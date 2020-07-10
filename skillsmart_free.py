cd0579640d809cae31a1797818de2e2074e14f80
Hamit <hamitka@list.ru>
2020-07-10 14:29:54 +0500

    odometr

--- skillsmart_free.py
+++ skillsmart_free.py
@@ -1,11 +1,7 @@
-def squirrel(x):
-    if x>=0:
-        fac = 1
-        for i in range(1, x+1):
-            fac = fac*i
-        faclist = [f for f in str(fac)]
-        return int(faclist[0])
-    else:
-        return None
+def odometer(listOksana):
+    listSpeed = [i for i in listOksana[::2]]
+    return sum(listSpeed)
 
+# listTest = [10,1,20,2, 30, 3, 40, 4]
+# print (odometer(listTest))
 # print(squirrel(int(input())))
