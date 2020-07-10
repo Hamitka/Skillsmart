ee61036f5f0738bc555fc53aa1100cdf4ffdd52e
Hamit <hamitka@list.ru>
2020-07-10 15:29:02 +0500

    odometr_v2

--- skillsmart_free.py
+++ skillsmart_free.py
@@ -1,7 +1,9 @@
 def odometer(listOksana):
     listSpeed = [i for i in listOksana[::2]]
-    return sum(listSpeed)
+    listHour = [0] + [i for i in listOksana[1::2]]
+    listHour = [listHour[i] - listHour[i-1] for i in range(1, len(listHour))]
+    return sum([listSpeed[i]*listHour[i] for i in range(len(listHour))])
 
-# listTest = [10,1,20,2, 30, 3, 40, 4]
+# listTest = [20,2,30,6,10,7]
 # print (odometer(listTest))
 # print(squirrel(int(input())))
