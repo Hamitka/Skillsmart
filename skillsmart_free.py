e7aa89fc459648dfa467e21e89ca776087c565ec
Hamit <hamitka@list.ru>
2020-07-12 21:03:43 +0500

    AI for Oksana

--- skillsmart_free.py
+++ skillsmart_free.py
@@ -1,24 +1,6 @@
-def WordSearch(lenght, s, subs):
-    # listS = [s[x:x+lenght] for x in range(0, len(s), lenght)]
-    listS = [[word[x:x+lenght] for x in range(0, len(word), lenght)] for word in s.split()]
-    listS = [item for sublist in listS for item in sublist ]
-    listNewS=[]
-    listLenS = [(len(w)+1) for w in listS]
+def SumOfThe(N, listData):
+    for i in range(len(listData)):
+        if (listData[i] == sum(listData) - listData[i]):
+            return listData[i]
 
-    x=0
-    while x < len(listLenS):
-        y = 1
-        while sum(listLenS[x:x+y]) <= lenght+1:
-            y += 1
-            if y >= lenght/2:
-                break
-        listNewS+= [listS[x:x+y-1]]
-        x=x+y-1
-    listFind = [1 if subs in i else 0 for i in listNewS]
-
-    return listFind
-
-# lenghtE = 12
-# s = 'строка разбивается на набор строк     через выравнивание по заданной ширине.'
-# subs = 'на'
-# print (*WordSearch(lenghtE, s, subs))
+# print(SumOfThe(5, [100, -50, 10, -25, 90, -35, 90]))
