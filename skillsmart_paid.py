39aaabd395835bdff36f735cf12218be4d526213
Hamit <hamitka@list.ru>
2020-07-15 13:28:45 +0500

    save_toner

--- skillsmart_paid.py
+++ skillsmart_paid.py
@@ -1,35 +1,19 @@
-def TheRabbitsFoot(s, encode=True):
-    def listTransponse(listIn):
-        listOut = [[''] * len(listIn) for i in range(len(listIn) + 1)]
-        for i in range(len(listIn)):
-            for j in range(len(listIn[i])):
-                try:
-                    listOut[j][i] = listIn[i][j]
-                except IndexError:
-                    continue
-        listOut = [''.join(i) for i in listOut]
-        # print(listOut)
-        return listOut
+def PrintingCosts(lineS):
+    listOfOutgo = [' ', '0', '!', '9', '"', '6', '#', '24', '$', '29', '%', '22', '&', '24', "'", '3', '(', '12',
+                   ')', '12', '*', '17', '+', '13', ',', '7', '-', '7', '.', '4', '/', '10', '0', '22', '1', '19',
+                   '2', '22', '3', '23', '4', '21', '5', '27', '6', '26', '7', '16', '8', '23', '9', '26', ':', '8',
+                   ';', '11', '<', '10', '=', '14', '>', '10', '?', '15', '@', '32', 'A', '24', 'B', '29', 'C', '20',
+                   'D', '26', 'E', '26', 'F', '20', 'G', '25', 'H', '25', 'I', '18', 'J', '18', 'K', '21', 'L', '16',
+                   'M', '28', 'N', '25', 'O', '26', 'P', '23', 'Q', '31', 'R', '28', 'S', '25', 'T', '16', 'U', '23',
+                   'V', '19', 'W', '26', 'X', '18', 'Y', '14', 'Z', '22', '[', '18', '\\', '10', ']', '18', '^', '7',
+                   '_', '8', '`', '3', 'a', '23', 'b', '25', 'c', '17', 'd', '25', 'e', '23', 'f', '18', 'g', '30',
+                   'h', '21', 'i', '15', 'j', '20', 'k', '21', 'l', '16', 'm', '22', 'n', '18', 'o', '20', 'p', '25',
+                   'q', '25', 'r', '13', 's', '21', 't', '17', 'u', '17', 'v', '13', 'w', '19', 'x', '13', 'y', '24',
+                   'z', '19', '{', '18', '|', '12', '}', '18', '~', '9']
+    dictOfOutgo = dict(zip(listOfOutgo[::2], listOfOutgo[1::2]))
+    listOfSymbol = list(lineS)
+    listOfToner = [int(dictOfOutgo[key]) if key in dictOfOutgo.keys() else 23 for key in listOfSymbol]
 
-    if encode == True:
-        s = s.replace(' ', '').strip()
-        sqrtS = len(s) ** .5
-        upLimit = int(sqrtS + 3 / 10 + 3 / 10 + 3 / 10 + 0.1)
-        listMatrixS = [list(s[x:x + upLimit]) for x in range(0, len(s), upLimit)]
-        listCode = listTransponse(listMatrixS)
+    return sum(listOfToner)
 
-        # print(listMatrixS)
-        return ' '.join(listCode).strip()
-    else:
-        listDeCode = listTransponse(s.split())
-        return ''.join(listDeCode).strip()
-
-
-# s = 'отдай мою кроличью лапку, щенок, она нужна'
-# lenS = len(s.replace(' ', ''))
-# print(lenS, lenS ** .5)
-#
-# print(TheRabbitsFoot(s, False))
-# print(TheRabbitsFoot(TheRabbitsFoot(s, True), False))
-# print (s.replace(' ', '').strip())
-# print ((TheRabbitsFoot(TheRabbitsFoot(s, True), False))==(s.replace(' ', '').strip()))
+# print (PrintingCosts('bla bla, bl@ b;@'))
