85b4094d50ed10e505696e73d9d5f65022edd370
Hamit <hamitka@list.ru>
2020-07-16 21:55:21 +0500

    Unmanned_traffic_lighter_v2

--- skillsmart_paid.py
+++ skillsmart_paid.py
@@ -1,19 +1,24 @@
 def Unmanned(L, N, traffic_light):
     def traffLighterRed(wayLenght, traffOneL):
-        time = traffOneL[1]
-        way = [i for i in range(1, wayLenght+1)]
-        traff  = [list(way[x:x+time]) for x in range(0, wayLenght, time)]
+        timeRed = traffOneL[1]
+        timeGreen = traffOneL[2]
+        way = [i for i in range(1, wayLenght + 1)]
+        traff = [list(way[x:x + timeRed]) for x in range(0, wayLenght, timeRed + timeGreen)]
         traffRed = [item for sublist in traff[0::2] for item in sublist]
         return traffRed
+
     timeWay = 0
     for tl in traffic_light:
         lighter = traffLighterRed(L, tl)
-        if tl[0]+timeWay in lighter:
-            timeWay+=max(lighter)-tl[0]
+        if tl[0] + timeWay in lighter:
+            timeWay += tl[1] - tl[0]
 
-    return timeWay+L
+    return timeWay + L
+
 
 # L1 = 10
-# N1 = 2
-# traffic_l1 = [[5, 5, 5], [2, 2, 2]]
+# N1 = 1
+# traffic_l1 = [[3, 5, 5], [5, 2, 2]]
+# traffic_l2 = [[5, 55, 5]]
 # print(Unmanned(L1, N1, traffic_l1))
+# print(Unmanned(L1, N1, traffic_l2))
