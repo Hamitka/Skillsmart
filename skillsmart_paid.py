5e8c647d194c9bd9e28af1a90b4cf71856a0dae8
Hamit <hamitka@list.ru>
2020-07-18 00:25:20 +0500

    ShopOLAP

--- skillsmart_paid.py
+++ skillsmart_paid.py
@@ -1,30 +1,12 @@
-def LineAnalysis(S):
-    listPattern = list(S)
-    if '.' not in listPattern:
-        return True
-    listPatternTemp = listPattern[1::]
-    listOfListPattern = []
-    y=0
-    for i, item in enumerate(listPatternTemp):
-       if item == '*':
-           listOfListPattern += [listPatternTemp[y:i+1]]
-           y=i+1
-    setPattern = set(tuple(i) for i in listOfListPattern)
-    if len(setPattern) <=1:
-        return True
-    else:
-        return False
+def ShopOLAP(N, items):
+    items = [int(i) if i.isdigit() else i for s in items for i in s.split()]
+    items = [items[x:x+2] for x in range(0, len(items), 2)]
+    items.sort()
+    dictItems = {key[0]: (sum(items[i][1] for i in range(len(items)) if items[i][0] == key[0])) for key in items}
+    listOut = list(dictItems.items())
+    return listOut
 
-# s1 = '*..*..*..*..*..*..*'
-# s2 = '*..*...*..*..*..*..*'
-# s3 = '*..*..*..*..*..**..*'
-# s4 = '*.......*.......*'
-# s5 = '*.......*........*'
-# s6 = '****'
-#
-# print(LineAnalysis(s1))
-# print(LineAnalysis(s2))
-# print(LineAnalysis(s3))
-# print(LineAnalysis(s4))
-# print(LineAnalysis(s5))
-# print(LineAnalysis(s6))
+# # list1 = [['платье1', 5], ['сумка32', 2], ['платье1', 1], ['сумка23', 2], ['сумка128', 4]]
+# list1 = ['платье1 5', 'сумка32 2', 'платье1 1', 'сумка23 2', 'сумка128 4']
+# N1 = len(list1)
+# print(ShopOLAP(N1, list1))
