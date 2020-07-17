20c1804a126d0883fa619f056da0c1d431df2d01
Hamit <hamitka@list.ru>
2020-07-17 22:22:43 +0500

    LineAnalysis

--- skillsmart_paid.py
+++ skillsmart_paid.py
@@ -1,12 +1,30 @@
-def MaximumDiscount(N, price):
-    price.sort()
-    price = price[::-1]
-    lenP = len(price)
-    listDiscount = [price[x:x + 3] for x in range(0, lenP, 3)]
-    listDiscountSum = [min(sublist) for sublist in listDiscount if len(sublist) == 3]
-    # listDiscountSum = [min(sublist) if len(sublist) == 3 else -sum(sublist) for sublist in listDiscount]
-    return sum(listDiscountSum)
+def LineAnalysis(S):
+    listPattern = list(S)
+    if '.' not in listPattern:
+        return 'да'
+    listPatternTemp = listPattern[1::]
+    listOfListPattern = []
+    y=0
+    for i, item in enumerate(listPatternTemp):
+       if item == '*':
+           listOfListPattern += [listPatternTemp[y:i+1]]
+           y=i+1
+    setPattern = set(tuple(i) for i in listOfListPattern)
+    if len(setPattern) <=1:
+        return 'да'
+    else:
+        return 'нет'
 
-# price1 = [250, 100, 300, 400, 200, 150, 350]
-# N1 = len(price1)
-# print(MaximumDiscount(N1, price1))
+# s1 = '*..*..*..*..*..*..*'
+# s2 = '*..*...*..*..*..*..*'
+# s3 = '*..*..*..*..*..**..*'
+# s4 = '*.......*.......*'
+# s5 = '*.......*........*'
+# s6 = '****'
+#
+# print(LineAnalysis(s1))
+# print(LineAnalysis(s2))
+# print(LineAnalysis(s3))
+# print(LineAnalysis(s4))
+# print(LineAnalysis(s5))
+# print(LineAnalysis(s6))
