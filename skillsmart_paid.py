6f647e9496688680e37766d5e19132c69164e974
Hamit <hamitka@list.ru>
2020-07-18 00:28:20 +0500

    ShopOLAP

--- skillsmart_paid.py
+++ skillsmart_paid.py
@@ -3,7 +3,8 @@ def ShopOLAP(N, items):
     items = [items[x:x+2] for x in range(0, len(items), 2)]
     items.sort()
     dictItems = {key[0]: (sum(items[i][1] for i in range(len(items)) if items[i][0] == key[0])) for key in items}
-    listOut = list(dictItems.items())
+    listOut = [key + ' ' + str(value) for key, value in dictItems.items()]
+
     return listOut
 
 # # list1 = [['платье1', 5], ['сумка32', 2], ['платье1', 1], ['сумка23', 2], ['сумка128', 4]]
