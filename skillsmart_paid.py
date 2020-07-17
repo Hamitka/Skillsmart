f6428da841ccff178a9c67ed55012583c7271117
Hamit <hamitka@list.ru>
2020-07-17 12:45:42 +0500

    Unmanned_traffic_lighter_v4

--- skillsmart_paid.py
+++ skillsmart_paid.py
@@ -1,12 +1,14 @@
 def Unmanned(L, N, traffic_light):
     timeWay = 0
     for tl in traffic_light:
-        if (tl[0] + timeWay)%(tl[1]+tl[2]) in range (1, tl[1]+1):
-            timeWay += (tl[1] - (tl[0] + timeWay)%(tl[1]+tl[2]))
+        if tl[0] >= L:
+            return L
+        elif (tl[0] + timeWay) % (tl[1] + tl[2]) in range(1, tl[1] + 1):
+            timeWay += (tl[1] - (tl[0] + timeWay) % (tl[1] + tl[2]))
 
     return timeWay + L
 
-#
+
 # L1 = 10
 # N1 = 1
 # traffic_l1 = [[3, 5, 5], [5, 2, 2]]
@@ -14,8 +16,10 @@ def Unmanned(L, N, traffic_light):
 # traffic_l3 = [[3, 6, 2], [6, 2, 2]]
 # traffic_l4 = [[6, 2, 2]]
 # traffic_l5 = [[1, 2, 2]]
+# traffic_l6 = [[11, 5, 5], [15, 2, 2]]
 # print(Unmanned(L1, N1, traffic_l1))
 # print(Unmanned(L1, N1, traffic_l2))
 # print(Unmanned(L1, N1, traffic_l3))
 # print(Unmanned(L1, N1, traffic_l4))
 # print(Unmanned(L1, N1, traffic_l5))
+# print(Unmanned(L1, N1, traffic_l6))
