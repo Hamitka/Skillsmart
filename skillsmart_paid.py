75e0a53e13678f942a69c0c66d5f072d280d41a8
Hamit <hamitka@list.ru>
2020-08-08 14:54:26 +0500

    TransformTransform

--- skillsmart_paid.py
+++ skillsmart_paid.py
@@ -1,40 +1,12 @@
-def MatrixTurn(lst:list, M:int, N:int, T:int):
-    # print (id(lst), *lst, sep='\n')
-    # print()
-    lstRotate = [list(i) for i in lst]
-    # print(*lstRotate, sep='\n')
+def TransformTransform(lst:list, N:int):
+    def sA(lstA:list):
+        lstB = []
+        N = len(lstA)
+        for i in lstA:
+            for j in range(N-i):
+                k = i + j
+                lstB += [max(lstA[j:k])]
+        return lstB
+    return sum(sA(sA(lst)))//2 == 0
 
-    def kontur(lst):
-        n, m = len(lst), len(lst[0])
-        numK = min(n, m)//2
-        lstOfKontur = [[] for _ in range(numK)]
-        lstOfXYKontur = [[] for _ in range(numK)]
-        for k in range(numK):
-            dx, dy = n-k-1, m-k-1
-            for i in range(k, dx):
-                lstOfKontur[k] += lst[i][k]
-                lstOfXYKontur[k] += [(i, k)]
-            for j in range(k, dy):
-                lstOfKontur[k] += lst[dx][j]
-                lstOfXYKontur[k] += [(dx, j)]
-            for i in range(dx, k, -1):
-                lstOfKontur[k] += lst[i][dy]
-                lstOfXYKontur[k] +=[(i, dy)]
-            for j in range(dy, k, -1):
-                lstOfKontur[k] += lst[k][j]
-                lstOfXYKontur[k] += [(k, j)]
-        return lstOfKontur, lstOfXYKontur
-    lstKontur, lstXYKontur = kontur(lstRotate)
-    lstKontur = [sublist[T:]+sublist[:T] for sublist in lstKontur]
-
-    for k, val in enumerate(lstXYKontur):
-        for u, tup in enumerate(val):
-            lstRotate[tup[0]][tup[1]] = lstKontur[k][u]
-    # print(*lstRotate, sep='\n')
-    lst[:]= [''.join(sublist) for sublist in lstRotate]
-#     print(id(lst), *lst, sep='\n')
-#     print()
-#
-# listTest = ["023456", "234567", "345678", "156789"]
-# MatrixTurn(listTest, 4, 6, 3)
-# print (id(listTest), *listTest, sep='\n')
+# print(TransformTransform([1, 2, 3, 4, 5, 6, 7], 7))
