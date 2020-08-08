6a895fda2bcea1216bd59f4f1ded2da2c0fcb77e
Hamit <hamitka@list.ru>
2020-08-09 02:07:29 +0500

    TransformTransform_v2

--- skillsmart_paid.py
+++ skillsmart_paid.py
@@ -2,11 +2,12 @@ def TransformTransform(lst:list, N:int):
     def sA(lstA:list):
         lstB = []
         N = len(lstA)
-        for i in lstA:
+        for i in range(N):
             for j in range(N-i):
                 k = i + j
-                lstB += [max(lstA[j:k])]
+                lstB += [max(lstA[j:k+1])]
         return lstB
-    return sum(sA(sA(lst)))//2 == 0
+    return sum(sA(sA(lst)))%2 == 0
 
 # print(TransformTransform([1, 2, 3, 4, 5, 6, 7], 7))
+# print(TransformTransform([3, 2, 1], 7))
