e6821e58829088e1a5b6b920ec06ca8c47b2a44a
Hamit <hamitka@list.ru>
2020-09-03 20:06:22 +0500

    string in string v5

--- skillsmart_paid_2.py
+++ skillsmart_paid_2.py
@@ -5,6 +5,21 @@ f("12345", "234") = True
 f("12345", "235") = False
 Для решения стандартные возможности типа s1 in s2 использовать нельзя, только условиями и циклами."""
 
+def in_string3(string1: str, string2: str):
+    len_str1, len_str2 = len(string1), len(string2)
+    if len_str2 > len_str1:
+        return False
+    elif string1 == string2:
+        return True
+    for i in range(len_str1):
+        if string1[i] == string2[0] and len_str1-i >= len_str2:
+            for j in range(1, len_str2):
+                if string1[i+j] != string2[j]:
+                    break
+                if j == len_str2-1:
+                    return True
+    return False
+
 def in_string2(string1: str, string2: str):
     len_str1, len_str2 = len(string1), len(string2)
     if len_str2 > len_str1:
@@ -35,16 +50,29 @@ def in_string(string1: str, string2: str):
 
 print(in_string('12345', '234'))
 print(in_string2('12345', '234'))
+print(in_string3('12345', '234'))
+
 print(in_string('1234511111111111', '1111'))
 print(in_string2('1234511111111111', '1111'))
+print(in_string3('1234511111111111', '1111'))
+
 print(in_string('12345', '2345'))
 print(in_string2('12345', '2345'))
+print(in_string3('12345', '2345'))
+
 print(in_string('12345', '235'))
 print(in_string2('12345', '235'))
+print(in_string3('12345', '235'))
+
 print(in_string('345', '12345'))
 print(in_string2('345', '12345'))
+print(in_string3('345', '12345'))
+
 print(in_string('345678', '56789'))
 print(in_string2('345678', '56789'))
+print(in_string3('345678', '56789'))
+
 print(in_string('допдопа', 'допа'))
 print(in_string2('допдопа', 'допа'))
+print(in_string3('допдопа', 'допа'))
 
