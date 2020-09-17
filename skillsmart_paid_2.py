333d1d7c359de0dcee1b4518f7899650a9186925
Hamit <hamitka@list.ru>
2020-09-18 00:38:01 +0500

    work with docx, xslx

--- skillsmart_paid_2.py
+++ skillsmart_paid_2.py
@@ -1,29 +1,45 @@
-"""4. Задания
-4.1. Напишите функцию, которая возвращает два списка: все файлы в текущем каталоге, включая файлы из его подкаталогов произвольной вложенности,
-и все каталоги в текущем каталоге, включая подкаталоги произвольной вложенности.
+"""3. Задания
+3.1. Напишите функцию, которая получает на вход имя файла -
+документа Word, и две строки, и выполняет в документе замену одной строки на другую.
+"""
 
-4.2. Напишите функцию, которая удаляет каталог (возможно, непустой) и все файлы внутри него.
-Если внутри каталога есть подкаталоги, ничего удалять не надо. """
 import os
-def dir_all(dir=os.getcwd()):
-    if not os.path.isdir(dir):
-        return None, 'No such directory'
-    lst_files, lst_dir = [], []
-    for i in os.walk(dir):
-        for k in i[2]:
-            lst_files += [i[0]+'\\'+ k]
-        for k in i[1]:
-            lst_dir += [i[0]+'\\'+ k]
-    return lst_files, lst_dir
-
-def delenddir(dir):
-    lst_of_files, lst_of_dir = dir_all(dir)
-    if len(lst_of_dir) > 0:
-        return None, 'is some dir in dir'
-    for f in lst_of_files:
-        os.remove(f)
-    os.rmdir(dir)
+import random
+import pandas as pd
+from docx import Document
+from openpyxl import Workbook, load_workbook
 
 
-# print(dir_all('own_main\\test'))
-delenddir('own_main\\test')
+def doc_replace_string(filename: str, str_find: str, str_replace: str):
+    """ Function to replace one string to another in some *.docx file """
+
+    if os.path.isfile:
+        document = Document(filename)
+    else:
+        return False
+    for p in document.paragraphs:
+        if p.text.find(str_find) != -1:
+            p.text = p.text.replace(str_find, str_replace)
+            # print(len(p.text), p.text)
+    document.save(filename)
+    return True
+
+# doc_replace_string('some_files\\test.docx', 'программистом', 'хорошим программистом')
+# doc_replace_string('some_files\\test.docx', 'хорошим', 'профессиональным')
+
+
+""" 3.2. Заполните в Excel-файле на любом листе квадрат 12*12 ячеек случайными значениями. """
+
+filename = 'some_files\\test.xlsx'
+wb = load_workbook(filename)
+ws = wb['Лист1']
+for i in range(1, 13):
+    for j in range(1, 13):
+        ws.cell(i, j).value = random.randint(0, 1000)
+wb.save(filename)
+
+# но по мне так лучше использовать pandas для таких целей:
+filename1 = 'some_files\\test1.xlsx'
+lst_out = [[random.randint(0, 1000) for i in range(12)] for j in range(12)]
+df = pd.DataFrame(lst_out)
+df.to_excel(filename1, header=False, index=False)
