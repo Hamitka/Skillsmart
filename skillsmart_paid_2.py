f30ea716a6e8a466889aa9657bf5d5fd62e10073
Hamit <hamitka@list.ru>
2020-09-22 00:44:13 +0500

    create new zip file with given extension

--- skillsmart_paid_2.py
+++ skillsmart_paid_2.py
@@ -1,45 +1,25 @@
-"""3. Задания
-3.1. Напишите функцию, которая получает на вход имя файла -
-документа Word, и две строки, и выполняет в документе замену одной строки на другую.
-"""
-
-import os
-import random
-import pandas as pd
-from docx import Document
-from openpyxl import Workbook, load_workbook
+""" Задание
+Напишите функцию, которая получает на вход два параметра:
+имя файла архива и расширение файла, сканирует текущий каталог в поисках файлов с подходящим расширением,
+и добавляет их в архив (исходно этот архив не существует). """
+import os.path
+from zipfile import ZipFile
 
 
-def doc_replace_string(filename: str, str_find: str, str_replace: str):
-    """ Function to replace one string to another in some *.docx file """
-
-    if os.path.isfile:
-        document = Document(filename)
-    else:
+def extfiletozip(zip_name: str, extention: str) -> bool:
+    """create new zip file with all files in current directory and given extension"""
+    lst_files = []
+    for root_dir_files in os.walk(os.getcwd()):
+        for file in root_dir_files[2]:
+            if os.path.splitext(file)[1] == extention and os.path.isfile(file):
+                lst_files += [file]
+    if not lst_files:
         return False
-    for p in document.paragraphs:
-        if p.text.find(str_find) != -1:
-            p.text = p.text.replace(str_find, str_replace)
-            # print(len(p.text), p.text)
-    document.save(filename)
+    # print (*lst_files, sep='\n')
+    with ZipFile(zip_name, 'w') as filezip:
+        for f in lst_files:
+            filezip.write(f)
     return True
 
-# doc_replace_string('some_files\\test.docx', 'программистом', 'хорошим программистом')
-# doc_replace_string('some_files\\test.docx', 'хорошим', 'профессиональным')
 
-
-""" 3.2. Заполните в Excel-файле на любом листе квадрат 12*12 ячеек случайными значениями. """
-
-filename = 'some_files\\test.xlsx'
-wb = load_workbook(filename)
-ws = wb['Лист1']
-for i in range(1, 13):
-    for j in range(1, 13):
-        ws.cell(i, j).value = random.randint(0, 1000)
-wb.save(filename)
-
-# но по мне так лучше использовать pandas для таких целей:
-filename1 = 'some_files\\test1.xlsx'
-lst_out = [[random.randint(0, 1000) for i in range(12)] for j in range(12)]
-df = pd.DataFrame(lst_out)
-df.to_excel(filename1, header=False, index=False)
+extfiletozip('test.zip', '.py')
