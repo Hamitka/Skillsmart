423571372abe1e69eeeefc29ff4c2094f4fff154
Hamit <hamitka@list.ru>
2020-11-08 21:04:25 +0500

    skillsmart XML lesson2

--- skillsmart_paid_2.py
+++ skillsmart_paid_2.py
@@ -1,93 +1,58 @@
-"""
- 5.3. Задания
-5.3.1. Условимся, что в нашем демонстрационном XML-документе только два уровня вложенности (languages и pc).
-Выведите на консоль содержание каждого узла как первого, так и второго уровня, включая название тега, список атрибутов и значение.
-5.3.2. Пользуясь только сведениями из данного занятия, напишите функцию, которая формирует список всех значений (по всем узлам)
-для конкретного тега, если задано его название (например, на вход функции подаётся "sex" или "pc_item").
-5.3.3. Напишите функцию, которая возвращает количество узлов в документе, включая дочерние, оснащённые заданным атрибутом.
-Прим. Наличие ключа в словаре можно проверить так (тестом наличия значения в списке ключей словаря):
-значение in имя-словаря.keys()
-Например:
-"name" in languages[i].attrib.keys()
-5.3.4. Напишите тесты к двум предыдущим заданиям.
+""" 6.6. Задания
+6.6.1. Напишите функцию, которая формирует список всех узлов по заданному тегу независимо от их глубины в XML-документе.
+На вход функция получает корневой узел.
+6.6.2. Напишите функцию, которая находит родителя заданного узла.
+6.6.3. Напишите функцию, которая удаляет все узлы по заданному тегу независимо от их глубины в XML-документе.
+6.6.4. Для трёх предыдущих функций напишите тесты.
 """
 
 import xml.etree.ElementTree as ETree
 
-# 5.3.1. Условимся, что в нашем демонстрационном XML-документе только два уровня вложенности (languages и pc).
-# Выведите на консоль содержание каждого узла как первого, так и второго уровня, включая название тега, список атрибутов и значение.
 xml1 = ETree.parse('demo.xml')
 root = xml1.getroot()
-for item in root:
-    if not len(item):
-        print(item.tag, item.attrib, item.text)
-    else:
-        for subitem in item:
-            print(subitem.tag, subitem.attrib, subitem.text)
 
 
-# тоже самое через рекурсию:
-def item_print(xml_root):
-    for item in xml_root:
-        if not len(item):
-            print(item.tag, item.attrib, item.text)
-        else:
-            item_print(item)
+# 6.6.1. Напишите функцию, которая формирует список всех узлов по заданному тегу независимо от их глубины в XML-документе.
+# На вход функция получает корневой узел.
+
+# через .iter
+def get_xml_node(xml_root, xml_tag: str):
+    xml_nodes = []
+    for item in xml_root.iter():
+        if item.tag == xml_tag:
+            xml_nodes += [item]
+    return xml_nodes
 
 
-# item_print(root)
-
-
-# 5.3.2. Пользуясь только сведениями из данного занятия, напишите функцию, которая формирует список всех значений (по всем узлам)
-# для конкретного тега, если задано его название (например, на вход функции подаётся "sex" или "pc_item").
-
-def get_xml_value(xml_root, xml_tag: str = None):
-    xml_attrib = []
+# через рекурсию
+def get_xml_node_rec(xml_root, xml_tag: str):
+    xml_nodes = []
 
     def get_tag(xml_root):
         for item in xml_root:
             if not len(item):
                 if item.tag == xml_tag:
-                    xml_attrib.append(item.text)
+                    xml_nodes.append(item)
             else:
                 get_tag(item)
 
-    if not xml_tag:
-        return None
-    else:
-        get_tag(xml_root)
-    return xml_attrib
+    get_tag(xml_root)
+    return xml_nodes
 
 
-# print(get_xml_value(root, 'pc_item'))
+# 6.6.2. Напишите функцию, которая находит родителя заданного узла.
+def get_xml_parent(xml_root, xml_tag: str):
+    for item in xml_root.iter():
+        if item.find(xml_tag) is not None:
+            return item
+    return None
 
 
-# 5.3.3. Напишите функцию, которая возвращает количество узлов в документе, включая дочерние, оснащённые заданным атрибутом.
+# 6.6.3. Напишите функцию, которая удаляет все узлы по заданному тегу независимо от их глубины в XML-документе.
+def del_xml_node(xml_root, xml_tag: str):
+    for item in xml_root.iter():
+        subitems = item.findall(xml_tag)
+        for i in subitems:
+            item.remove(i)
+    return xml_root
 
-def count_xml_attrib(xml_root, xml_attrib: str):
-    xml_count = 0
-    # через рекусрию:
-    # def parse_xml_attrinb(xml_root):
-    #     nonlocal xml_count
-    #     for item in xml_root:
-    #         if not len(item):
-    #             if xml_attrib in item.attrib.keys():
-    #                 xml_count += 1
-    #         else:
-    #             parse_xml_attrinb(item)
-
-    if not xml_attrib:
-        return 0
-    else:
-        for item in xml_root:
-            if not len(item):
-                if xml_attrib in item.attrib.keys():
-                    xml_count += 1
-            else:
-                for subitem in item:
-                    if xml_attrib in subitem.attrib.keys():
-                        xml_count += 1
-    return xml_count
-
-
-# print(count_xml_attrib(root, 'name'))
--- skillsmart_paid_2_unittest.py
+++ skillsmart_paid_2_unittest.py
@@ -2,18 +2,38 @@ import unittest
 import skillsmart_paid_2 as ssp2
 
 class MyTestCase(unittest.TestCase):
-    def test_xml_value(self):
-        self.assertEqual(len(ssp2.get_xml_value(ssp2.root, 'name')), 1)
-        self.assertEqual(len(ssp2.get_xml_value(ssp2.root, 'age')), 1)
-        self.assertEqual(len(ssp2.get_xml_value(ssp2.root, 'sex')), 1)
-        self.assertEqual(len(ssp2.get_xml_value(ssp2.root, 'pc_item')), 4)
-        self.assertEqual(len(ssp2.get_xml_value(ssp2.root, 'language')), 3)
-        self.assertEqual(ssp2.get_xml_value(ssp2.root, 'pc_item'), ['Linux', 'Intel Core i7-8700', '64', '5000'])
+    ## 6.6.1. Напишите функцию, которая формирует список всех узлов по заданному тегу независимо от их глубины в XML-документе.
+    # На вход функция получает корневой узел.
+    def test_xml_list_node(self):
+        self.assertEqual(len(ssp2.get_xml_node(ssp2.root, 'sex')), 1)
+        self.assertEqual(len(ssp2.get_xml_node(ssp2.root, 'languages')), 1)
+        self.assertEqual(len(ssp2.get_xml_node(ssp2.root, 'language')), 3)
+        self.assertEqual(len(ssp2.get_xml_node(ssp2.root, 'pc')), 1)
+        self.assertEqual(len(ssp2.get_xml_node(ssp2.root, 'pc_item')), 4)
 
-    def test_count_xml_attrib(self):
-        self.assertEqual(ssp2.count_xml_attrib(ssp2.root, 'name'), 7)
-        self.assertEqual(ssp2.count_xml_attrib(ssp2.root, ''), 0)
+    #6.6.2. Напишите функцию, которая находит родителя заданного узла.
+    def test_xml_parrent(self):
+        self.assertEqual(ssp2.get_xml_parent(ssp2.root, 'sex').tag, 'data')
+        self.assertEqual(ssp2.get_xml_parent(ssp2.root, 'languages').tag, 'data')
+        self.assertEqual(ssp2.get_xml_parent(ssp2.root, 'language').tag, 'languages')
+        self.assertEqual(ssp2.get_xml_parent(ssp2.root, 'pc').tag, 'data')
+        self.assertEqual(ssp2.get_xml_parent(ssp2.root, 'pc_item').tag, 'pc')
+        self.assertEqual(ssp2.get_xml_parent(ssp2.root, ''), None)
+        self.assertEqual(ssp2.get_xml_parent(ssp2.root, 'blabla'), None)
 
+
+    # 6.6.3. Напишите функцию, которая удаляет все узлы по заданному тегу независимо от их глубины в XML-документе.
+    def test_xml_del_node(self):
+        tag1 = 'sex'
+        self.assertEqual(len(ssp2.root.findall(tag1)), 1)
+        ssp2.del_xml_node(ssp2.root, tag1)
+        self.assertEqual(len(ssp2.root.findall(tag1)), 0)
+        tag2 = 'language'
+        for sub in ssp2.root.iter('languages'):
+            self.assertEqual(len(sub.findall(tag2)), 3)
+        ssp2.del_xml_node(ssp2.root, tag2)
+        for sub in ssp2.root.iter('languages'):
+            self.assertEqual(len(sub.findall(tag2)), 0)
 
 
 if __name__ == '__main__':
