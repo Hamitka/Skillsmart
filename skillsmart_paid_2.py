b2f433978c08928c1d2b6fa5629f57711b5f68f7
Hamit <hamitka@list.ru>
2020-11-29 16:43:58 +0500

    skillsmart Thread one by one

--- skillsmart_paid_2.py
+++ skillsmart_paid_2.py
@@ -1,69 +1,44 @@
-"""
- Задание
-1. Самостоятельно разберитесь, как сделать из класса List2 итератор с конструктором,
-чтобы он работал не только начиная с 1, а с любого заданного значения, например:
-for n in List2(5): # начиная с 5
-    print(n)
-2. Напишите версию List2 с конструктором, который получает количество N итерируемых элементов (сейчас 10),
-и флажок конечности/бесконечности. В случае конечности List2 выдаёт N элементов и завершает работу,
-а в случае бесконечности начинает повторно выдавать свою последовательность с самого начала.
-3. Сделайте тесты для этих двух версий List2.
-"""
+""" Задание
+Напишите функцию, которая получает на вход большой список вещественных чисел
+(например, 10 тысяч элементов, сгенерируйте их случайным образом),
+и количество процессов (целое число), которое должно просуммировать этот массив по кусочкам.
+
+Например, задано количество процессов -- 10 и список из 100 000 элементов.
+Функция запускает 10 процессов, каждый из которых суммирует свой участок списка
+(по 10 000 элементов), суммирует их результаты и возвращает итоговую сумму всего списка.
+Не используйте в решении join().
+Проверьте корректность работы функции,
+сравнив её результат с результатом обычного суммирования массива циклом. """
+from random import random
+from threading import Thread
+import time
 
 
-class List2:
-    def __init__(self, begin):
-        self.begin = begin
-
-    def __iter__(self):
-        self.start = self.begin
-        self.count = 0
-        return self
-
-    def __next__(self):
-        current = self.start
-        self.start = self.start * 2
-        self.count += 1
-        if self.count <= 10:
-            return current
-        raise StopIteration
+def lst_sum(some_list, name='', result=None):
+    sum = 0
+    for v in some_list:
+        sum += v
+        # print(name)
+        # time.sleep(.00005)
+    if result is not None:
+        result[name] = sum
+    return sum
 
 
-class List2v2:
-    def __init__(self, begin, count_end, infinity=False):
-        self.begin = begin
-        self.count_end = count_end
-        self.infinity = infinity
+def lst_sum_thread(some_list, num_thread):
+    result_dct = {}
+    size_part = len(some_list) // num_thread
+    list_of_list = [some_list[i:i + size_part] for i in range(0, len(some_list), size_part)]
+    for i, lst in enumerate(list_of_list):
+        name = 'Thread N' + str(i)
+        some_thread = Thread(target=lst_sum, name=name, args=(lst, name, result_dct))
+        some_thread.start()
+        # print ('%s is start' % (name))
+    # print(result_dct)
+    sum_total = lst_sum(list(result_dct.values()))
+    return sum_total
 
-    def __iter__(self):
-        self.start = self.begin
-        self.count = 0
-        return self
+list_float = [random() for _ in range(100000)]
 
-    def __next__(self):
-        current = self.start
-        self.start = self.start * 2
-        self.count += 1
-        if (not self.infinity and self.count <= self.count_end) or (self.infinity and self.count < self.count_end):
-            return current
-        elif self.infinity:
-            self.count = 0
-            self.start = self.begin
-            return current
-        raise StopIteration
-
-# test_lst2 = List2(10)
-# test_iter_lst2 = iter(test_lst2)
-#
-# for i in test_lst2:
-#     print(i)
-# next(test_iter_lst2)
-#
-# test_lst2v2 = List2v2(10, 10, False)
-# test_iter_lst2v2 = iter(test_lst2v2)
-#
-# for i in test_iter_lst2v2:
-#     print(i)
-
-# for i in range(20):
-#     print(next(test_iter_lst2v2))
+print(lst_sum_thread(list_float, 10), sum(list_float))
+print(lst_sum_thread(list_float, 10) == sum(list_float))
