9fa7b5a71729d9d2c6227d4445499a9ded74b718
Hamit <hamitka@list.ru>
2021-05-23 17:18:07 +0300

    skillsmart LinkedList func v1

--- LinkedList.py
+++ LinkedList.py
@@ -35,11 +35,27 @@ class LinkedList:
         return None
 
     def find_all(self, val):
-        return [] # здесь будет ваш код
+        """
+        1.4. Добавьте в класс LinkedList метод поиска всех узлов по конкретному значению
+        (возвращается стандартный питоновский список найденных узлов).
+        """
+        result = []
+        node = self.head
+        while node is not None:
+            if node.value == val:
+                result.append(node)
+            node = node.next
+        return result
 
     def delete(self, val, all=False):
-        while self.head.value == val:
+        """
+        1.1. Добавьте в класс LinkedList метод удаления одного узла по его значению
+        где флажок all=False по умолчанию -- удаляем только первый нашедшийся элемент.
+        1.2. Дополните этот метод удалением всех узлов по конкретному значению (флажок all=True).
+        """
+        while self.head and self.head.value == val:
             self.head = self.head.next
+            if self.head is None: self.tail = None
             if not all:
                 return None
 
@@ -48,6 +64,8 @@ class LinkedList:
         while node is not None:
             if node.value == val:
                 node_prev.next = node.next
+                if self.head is None: self.tail = None
+                if node == self.tail: self.tail = node_prev
                 if not all:
                     return None
             else:
@@ -56,10 +74,47 @@ class LinkedList:
         return None
 
     def clean(self):
-        pass # здесь будет ваш код
+        """
+        1.3. Добавьте в класс LinkedList метод очистки всего содержимого
+        (создание пустого списка) -- clean()
+        """
+        self.__init__()
 
     def len(self):
-        return 0 # здесь будет ваш код
+        """
+        1.5. Добавьте в класс LinkedList метод вычисления текущей длины списка -- len()
+        """
+        i = 0
+        node = self.head
+        while node is not None:
+            i += 1
+            node = node.next
+        return i
 
     def insert(self, afterNode, newNode):
-        pass # здесь будет ваш код
+        """
+        1.6. Добавьте в класс LinkedList метод вставки узла newNode после заданного узла afterNode (из списка)
+        Если afterNode = None, добавьте новый элемент первым в списке.
+        """
+        if afterNode is None:
+            newNode.next = self.head
+            self.head = newNode
+            return None
+        node = self.head
+        while node is not None:
+            if node == afterNode:
+                newNode.next = node.next
+                node.next = newNode
+                if node == self.tail: self.tail = newNode
+                return None
+            node = node.next
+
+    def __add__(self, other):
+        if self.len() == other.len():
+            result = LinkedList()
+            node_self, node_other = self.head, other.head
+            while node_self:
+                result.add_in_tail(Node(node_self.value + node_other.value))
+                node_self, node_other = node_self.next, node_other.next
+            return result
+        return None
--- LinkedList_tests.py
+++ LinkedList_tests.py
@@ -5,19 +5,146 @@ import LinkedList as ll
 
 
 class MyTestCase(unittest.TestCase):
+    def compare_list_llist(self, some_list, some_llist):
+        node = some_llist.head
+        for i in some_list:
+            self.assertEqual(node.value, i)
+            # print(i, node.value)
+            node = node.next
+
     def test_linked_list_dell_all(self):
-        temp_list = [11, 11, 11, 11, 12, 23, 11, 11, 34, 45, 56, 67, 11, 78, 11, 89, 11, 11, 11]
         test_llist = ll.LinkedList()
+        self.assertEqual(None, test_llist.delete(1, all=True))
+
+        temp_list = [11, 11, 11, 11, 12, 23, 11, 11, 34, 45, 56, 67, 11, 78, 11, 89, 11, 11, 11]
         for i in temp_list:
             test_llist.add_in_tail(ll.Node(i))
         del_item = 11
-        test_llist.delete(del_item, True)
+        test_llist.delete(del_item, all=True)
         temp_list = [i for i in temp_list if i != del_item]
-        node = test_llist.head
+        self.compare_list_llist(temp_list, test_llist)
+        # delete all same node:
+        test_llist.clean()
+        for i in range(10):
+            test_llist.add_in_tail(ll.Node(del_item))
+        test_llist.delete(del_item, all=True)
+        self.assertEqual(None, test_llist.head)
+        self.assertEqual(None, test_llist.tail)
+        self.assertEqual(0, test_llist.len())
+
+    def test_linked_list_dell_one(self):
+        test_llist = ll.LinkedList()
+        self.assertEqual(None, test_llist.delete(1, all=False))
+
+        temp_list = [11, 11, 11, 11, 12, 23, 11, 11, 34, 45, 56, 67, 11, 78, 11, 89, 11, 11, 11]
         for i in temp_list:
-            self.assertEqual(i, node.value)
-            print(i, node.value)
-            node = node.next
+            test_llist.add_in_tail(ll.Node(i))
+        del_item = 11
+        test_llist.delete(del_item, all=False)
+        temp_list.remove(del_item)
+        self.compare_list_llist(temp_list, test_llist)
+
+    def test_linked_list_dell_first(self):
+        temp_list = [99, 11, 11, 12, 23, 11, 11, 34, 45, 56, 67, 11, 78, 11, 89, 11, 11, 11]
+        test_llist = ll.LinkedList()
+        for i in temp_list:
+            test_llist.add_in_tail(ll.Node(i))
+        del_item = 99
+        test_llist.delete(del_item, all=False)
+        temp_list.remove(del_item)
+        self.compare_list_llist(temp_list, test_llist)
+
+    def test_linked_list_dell_last(self):
+        temp_list = [12, 23, 11, 11, 34, 45, 56, 67, 11, 78, 11, 89, 11, 99]
+        test_llist = ll.LinkedList()
+        for i in temp_list:
+            test_llist.add_in_tail(ll.Node(i))
+        del_item = 99
+        test_llist.delete(del_item, all=False)
+        temp_list.remove(del_item)
+        self.compare_list_llist(temp_list, test_llist)
+        self.assertEqual(temp_list[-1], test_llist.tail.value)
+
+    def test_linked_list_dell_single(self):
+        test_llist = ll.LinkedList()
+        test_llist.add_in_tail(ll.Node(99))
+        item_del = 99
+        test_llist.delete(item_del, all=False)
+        self.assertEqual(None, test_llist.head)
+        self.assertEqual(None, test_llist.tail)
+
+    def test_linked_list_clean(self):
+        temp_list = [12, 23, 11, 11, 34, 45, 56, 67, 11, 78, 11, 89, 11, 11, 99]
+        test_llist = ll.LinkedList()
+        for i in temp_list:
+            test_llist.add_in_tail(ll.Node(i))
+        test_llist.clean()
+        self.assertEqual(None, test_llist.head)
+        self.assertEqual(None, test_llist.tail)
+
+    def test_linked_list_find_all(self):
+        temp_list = [12, 23, 11, 11, 34, 45, 56, 67, 11, 78, 11, 89, 11, 11, 99]
+        test_llist = ll.LinkedList()
+        for i in temp_list:
+            test_llist.add_in_tail(ll.Node(i))
+        item_find = 11
+        self.assertEqual(temp_list.count(item_find), len(test_llist.find_all(item_find)))
+        test_llist.clean()
+        self.assertEqual(0, len(test_llist.find_all(item_find)))
+
+    def test_linked_list_find_len(self):
+        temp_list = [12, 23, 11, 11, 34, 45, 56, 67, 11, 78, 11, 89, 11, 11, 99]
+        test_llist = ll.LinkedList()
+        for i in temp_list:
+            test_llist.add_in_tail(ll.Node(i))
+        self.assertEqual(len(temp_list), test_llist.len())
+        test_llist.clean()
+        self.assertEqual(0, test_llist.len())
+
+    def test_linked_list_insert(self):
+        temp_list = [12, 23, 34, 45, 56, 67, 78, 89]
+        test_llist = ll.LinkedList()
+        for i in temp_list:
+            test_llist.add_in_tail(ll.Node(i))
+        # insert into begin:
+        item_insert = 10
+        test_llist.insert(None, ll.Node(item_insert))
+        temp_list.insert(0, item_insert)
+        self.compare_list_llist(temp_list, test_llist)
+        self.assertEqual(temp_list[0], test_llist.head.value)
+        # insert into middle:
+        item_insert = 39
+        test_llist.insert(test_llist.find(34), ll.Node(item_insert))
+        temp_list.insert(4, item_insert)
+        self.compare_list_llist(temp_list, test_llist)
+        # insert into end:
+        item_insert = 99
+        test_llist.insert(test_llist.find(89), ll.Node(item_insert))
+        temp_list.append(item_insert)
+        self.compare_list_llist(temp_list, test_llist)
+        self.assertEqual(temp_list[-1], test_llist.tail.value)
+
+    def test_linked_list_addition(self):
+        temp_list_1 = [1, 2, 3, 4, 5, 6, 7, 8]
+        temp_list_2 = [10, 20, 30, 40, 50, 60, 70, 80]
+
+        temp_sum_lists = [a + b for a, b in zip(temp_list_1, temp_list_2)]
+
+        test_llist_1 = ll.LinkedList()
+        for i in temp_list_1:
+            test_llist_1.add_in_tail(ll.Node(i))
+
+        test_llist_2 = ll.LinkedList()
+        for i in temp_list_2:
+            test_llist_2.add_in_tail(ll.Node(i))
+
+        test_sum_llist = test_llist_1 + test_llist_2
+        self.compare_list_llist(temp_sum_lists, test_sum_llist)
+
+        # for linked list if different length:
+        test_llist_1.add_in_tail(ll.Node(9))
+        test_sum_llist_2 = test_llist_1 + test_llist_2
+        self.assertEqual(None, test_sum_llist_2)
 
 
 if __name__ == '__main__':
