7245627ada9208c26a7346883e7c27e88a1c8954
Hamit <hamitka@list.ru>
2021-06-06 16:45:17 +0300

    skillsmart Dequeue v1

--- Queue.py
+++ Queue.py
@@ -20,3 +20,45 @@ class Queue:
 def rotate(queue, num_elements):
     for i in range(num_elements):
         queue.enqueue(queue.dequeue())
+
+
+class Deque:
+    def __init__(self):
+        self.deque = []
+
+    def addFront(self, item):
+        self.deque.insert(0, item)
+
+    def addTail(self, item):
+        self.deque.append(item)
+
+    def removeFront(self):
+        if self.deque:
+            return self.deque.pop(0)
+        else:
+            return None
+
+    def removeTail(self):
+        if self.deque:
+            return self.deque.pop()
+        else:
+            return None
+
+    def size(self):
+        return len(self.deque)
+
+
+def palindrome(some_string):
+    deque = Deque()
+    for i in some_string:
+        if i != ' ':
+            deque.addTail(i)
+    while deque.size() > 1:
+        if deque.removeFront() != deque.removeTail():
+            return False
+    return True
+
+
+# print(palindrome('я иду с мечем судия'))
+# print(palindrome('лёша на полке клопа нашёл'))
+# print(palindrome('а роза упала на лапу азора'))
---
+++ Queue_tests.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+
+import unittest
+import Queue
+
+
+class MyTestCase(unittest.TestCase):
+    def test_deque(self):
+        deque = Queue.Deque()
+        deque.addTail(7)
+        self.assertEqual(1, deque.size())
+        deque.addTail(8)
+        self.assertEqual(2, deque.size())
+        deque.addTail(9)
+        self.assertEqual(3, deque.size())
+        deque.addFront(3)
+        self.assertEqual(4, deque.size())
+        deque.addFront(2)
+        self.assertEqual(5, deque.size())
+        deque.addFront(1)
+        self.assertEqual(6, deque.size())
+
+        self.assertEqual(9, deque.removeTail())
+        self.assertEqual(1, deque.removeFront())
+        self.assertEqual(4, deque.size())
+
+        self.assertEqual(8, deque.removeTail())
+        self.assertEqual(2, deque.removeFront())
+        self.assertEqual(2, deque.size())
+
+        self.assertEqual(7, deque.removeTail())
+        self.assertEqual(3, deque.removeFront())
+        self.assertEqual(0, deque.size())
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
