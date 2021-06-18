b593977d737871fc3ed1415e90f1253f4106c526
Hamit <hamitka@list.ru>
2021-06-18 23:01:11 +0300

    skillsmart NativeDictionary v1

--- HashTable.py
+++ HashTable.py
@@ -5,6 +5,7 @@ class HashTable:
     для экспериментов можно например брать 17 или 19),
     и длину шага (количество слотов) для поиска следующего свободного слота (например, 3)
     """
+
     def __init__(self, size, step):
         self.size = size
         self.step = step
@@ -17,7 +18,7 @@ class HashTable:
         # в качестве value поступают строки!
         # всегда возвращает корректный индекс слота
         # return hash(value) % self.size
-        return sum(value.encode('utf-8')) % self.size
+        return sum([ord(value[i]) * (i+1) for i in range(len(value))]) % self.size
 
     def seek_slot(self, value):
         """
@@ -58,4 +59,47 @@ class HashTable:
         # находит индекс слота со значением, или None
         if value in self.slots:
             return self.slots.index(value)
+        return None
+
+
+class NativeDictionary:
+    def __init__(self, size):
+        self.size = size
+        self.slots = [None] * self.size
+        self.values = [None] * self.size
+
+    def hash_fun(self, key):
+        # в качестве key поступают строки!
+        # всегда возвращает корректный индекс слота
+        return sum([ord(key[i]) * (i+1) for i in range(len(key))]) % self.size
+        # return sum(key.encode('utf-8')) % self.size
+
+    def is_key(self, key):
+        """
+        - is_key(key) - проверка, имеется ли в слотах такой ключ
+        """
+        # возвращает True если ключ имеется,
+        # иначе False
+        if key in self.slots:
+            return True
+        return False
+
+    def put(self, key, value):
+        """
+        - put(key, value) - сохранение внутри класса ассоциативного массива
+        пары ключ-значениепо описанной выше схеме;"""
+        # гарантированно записываем
+        # значение value по ключу key
+        index = self.hash_fun(key)
+        self.slots[index] = key
+        self.values[index] = value
+
+    def get(self, key):
+        """
+        - get(key) - поиск и извлечение значения по ключу, или None, если ключ не найден.
+        """
+        # возвращает value для key,
+        # или None если ключ не найден
+        if self.is_key(key):
+            return self.values[self.hash_fun(key)]
         return None
--- HashTable_tests.py
+++ HashTable_tests.py
@@ -24,6 +24,20 @@ class MyTestCase(unittest.TestCase):
 
         print(my_hash_table.find('test'))
 
+    def test_native_dict(self):
+        my_dict = ht.NativeDictionary(17)
+        for i in range(my_dict.size):
+            my_dict.put('key' + str(i), 'value' + str(i))
+            self.assertEqual(True, my_dict.is_key('key' + str(i)))
+            self.assertEqual('value' + str(i), my_dict.get('key' + str(i)))
+
+        print([f'{my_dict.slots[i]}: {my_dict.values[i]}' for i in range(my_dict.size)])
+        my_dict.put('key5', 'value555')
+        print([f'{my_dict.slots[i]}: {my_dict.values[i]}' for i in range(my_dict.size)])
+        self.assertEqual('value555', my_dict.get('key5'))
+        self.assertEqual(None, my_dict.get('key555'))
+
+
 
 if __name__ == '__main__':
     unittest.main()
